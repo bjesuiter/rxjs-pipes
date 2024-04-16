e234665ff5c98e10d264c93c419ca3ca7b80ef4a
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-04-16 19:08:02 +0200

    refine first test

--- packages/throttle-async-rxjs-pipe/src/throttle-async.test.ts
+++ packages/throttle-async-rxjs-pipe/src/throttle-async.test.ts
@@ -1,5 +1,12 @@
 import { expect, test } from "bun:test";
-import { EMPTY, firstValueFrom, interval, of, TimeoutError } from "rxjs";
+import {
+  EMPTY,
+  firstValueFrom,
+  interval,
+  lastValueFrom,
+  of,
+  TimeoutError,
+} from "rxjs";
 import { catchError, map, take, tap, timeout } from "rxjs/operators";
 import { throttleAsync } from "./throttle-async";
 
@@ -19,25 +26,69 @@ test(
     const testpipe = interval(500).pipe(
       tap(() => {
         absoluteTime += 500;
-        console.log(`Time Passed: ${absoluteTime}`);
+        console.log(`interval(500): Time Passed: ${absoluteTime}`);
       }),
       throttleAsync(waitUntil$),
       tap(() => {
         eventCount++;
-        console.log(`Emit after ThrottleAsync: ${absoluteTime}`);
+        console.log(`testpipe: Emits after ThrottleAsync: ${absoluteTime}`);
       }),
-      timeout(3000),
+      timeout(1500),
       catchError((error) => {
-        // THIS CANNOT HAPPEN IN THIS TEST!
         console.log(`TimeoutEvent: ${error.message}`);
         expect(error).toBeInstanceOf(TimeoutError);
         expect(eventCount).toBe(1);
-        console.clear();
-        return EMPTY;
+
+        // this last emission is necessary to prevent the test from hanging
+        // it is used with take(2) to complete the testpipe for lastValueFrom to resolve
+        return of("timeout");
       }),
     );
 
-    return firstValueFrom(testpipe);
+    return lastValueFrom(testpipe.pipe(take(2)));
   },
   3000,
 );
+
+/**
+ * Throttle Async Pipe (Source Interval 500)
+ * With one waitUntil$ emission after 2 sek
+ * Should have emitted two elements after 3 sek
+ *
+ * Emission Diagram:
+ * 500ms: interval(500) emits 1, testpipe emits
+ * 1000ms: interval(500) emits 2, but testpipe with throttleAsync waits for waitUntil$ to emit
+ * 1500ms: interval(500) emits 3, but ttestpipe with hrottleAsync waits for waitUntil$ to emit
+ * 2000ms: interval(500) emits 4, waitUntil$ emits, testpipe with throttleAsync emits
+ */
+
+test.todo(`ThrottleAsync: emits two events after waitUntil$ emits`, () => {
+  const waitUntil$ = interval(2000).pipe(
+    take(1),
+    tap(() => console.log("Observable waitUntil$ emitted!")),
+  );
+  let eventCount = 0;
+  let absoluteTime = 0;
+
+  const testpipe = interval(500).pipe(
+    tap(() => {
+      absoluteTime += 500;
+      console.log(`interval(500): Time Passed: ${absoluteTime}`);
+    }),
+    throttleAsync(waitUntil$),
+    tap(() => {
+      eventCount++;
+      console.log(`Emit after ThrottleAsync: ${absoluteTime}`);
+    }),
+    timeout(3000),
+    catchError((error) => {
+      // THIS SHOULD HAPPEN IN THIS TEST!
+      console.log(`TimeoutEvent: ${error.message}`);
+      expect(error).toBeInstanceOf(TimeoutError);
+      expect(eventCount).toBe(2);
+      return EMPTY;
+    }),
+  );
+
+  return firstValueFrom(testpipe);
+});
