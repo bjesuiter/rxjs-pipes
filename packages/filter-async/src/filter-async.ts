bb58dc39fc27cc7cab32f2d61ab6c6991acdd342
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-04-16 19:44:05 +0200

    reformat filter-async.ts

--- packages/filter-async/src/filter-async.ts
+++ packages/filter-async/src/filter-async.ts
@@ -8,13 +8,20 @@
  * @param predicate
  * @param thisArg
  */
-import { pipe, from, MonoTypeOperatorFunction, Observable, OperatorFunction, asyncScheduler } from 'rxjs';
-import { map, filter, concatMap, subscribeOn, mergeMap } from 'rxjs/operators';
-import { Thenable } from 'es6-promise';
+import {
+  asyncScheduler,
+  from,
+  MonoTypeOperatorFunction,
+  Observable,
+  OperatorFunction,
+  pipe,
+} from "rxjs";
+import { concatMap, filter, map, mergeMap, subscribeOn } from "rxjs/operators";
+import { Thenable } from "es6-promise";
 
 interface FilterContainer<T> {
-    filterResult: boolean,
-    entry: T
+  filterResult: boolean;
+  entry: T;
 }
 
 type Predicate$<T> = (value: T, index: number) => Observable<boolean>;
@@ -30,12 +37,18 @@ type Predicate<T> = (value: T, index: number) => Thenable<boolean>;
  * @param maxConcurrent A number indicating the max number of elements processed parallel
  *                      Warning: Only active when parallel is true!
  */
-export function filterByPromise<T>(predicate: Predicate<T>,
-                                   parallel: boolean = false,
-                                   maxConcurrent: number = Number.POSITIVE_INFINITY): MonoTypeOperatorFunction<T> {
-    return pipe(
-        filterAsync((data: T, index: number) => from(predicate(data, index)), parallel, maxConcurrent)
-    );
+export function filterByPromise<T>(
+  predicate: Predicate<T>,
+  parallel: boolean = false,
+  maxConcurrent: number = Number.POSITIVE_INFINITY,
+): MonoTypeOperatorFunction<T> {
+  return pipe(
+    filterAsync(
+      (data: T, index: number) => from(predicate(data, index)),
+      parallel,
+      maxConcurrent,
+    ),
+  );
 }
 
 /**
@@ -48,12 +61,15 @@ export function filterByPromise<T>(predicate: Predicate<T>,
  * @param maxConcurrent A number indicating the max number of elements processed parallel
  *                      Warning: Only active when parallel is true!
  */
-export function filterAsync<T>(predicate: Predicate$<T>,
-                               parallel: boolean,
-                               maxConcurrent: number = Number.POSITIVE_INFINITY): MonoTypeOperatorFunction<T> {
-    return (parallel) ? filterAsyncSequential(predicate) : filterAsyncParallel(predicate, maxConcurrent);
+export function filterAsync<T>(
+  predicate: Predicate$<T>,
+  parallel: boolean,
+  maxConcurrent: number = Number.POSITIVE_INFINITY,
+): MonoTypeOperatorFunction<T> {
+  return parallel
+    ? filterAsyncSequential(predicate)
+    : filterAsyncParallel(predicate, maxConcurrent);
 }
-
 
 /**
  * This rxjs 6+ pipe uses concatMap to apply the async predicate function to each data entry.
@@ -61,17 +77,18 @@ export function filterAsync<T>(predicate: Predicate$<T>,
  * * Runs sequential when data comes in faster than the filter function can process it. (Because of ConcatMap)
  * @param predicate A predicate function to test each event which returns Thenable<boolean>
  */
-function filterAsyncSequential<T>(predicate: Predicate$<T>): MonoTypeOperatorFunction<T> {
-    return pipe(
-        concatMap((data: T, index: number) => {
-                // run the predicate &
-                // put the predicate result + the data entry together into container
-                return predicate(data, index)
-                    .pipe(map((isValid) => ({filterResult: isValid, entry: data})));
-            }
-        ),
-        filterDataAndRemoveContainer()
-    );
+function filterAsyncSequential<T>(
+  predicate: Predicate$<T>,
+): MonoTypeOperatorFunction<T> {
+  return pipe(
+    concatMap((data: T, index: number) => {
+      // run the predicate &
+      // put the predicate result + the data entry together into container
+      return predicate(data, index)
+        .pipe(map((isValid) => ({ filterResult: isValid, entry: data })));
+    }),
+    filterDataAndRemoveContainer(),
+  );
 }
 
 /**
@@ -82,28 +99,32 @@ function filterAsyncSequential<T>(predicate: Predicate$<T>): MonoTypeOperatorFun
  * @param maxConcurrent A number indicating the max number of elements processed parallel
  *                      Warning: Only active when parallel is true!
  */
-function filterAsyncParallel<T>(predicate: Predicate$<T>, maxConcurrent: number = Number.POSITIVE_INFINITY): MonoTypeOperatorFunction<T> {
-    return pipe(
-        mergeMap((data: T, index: number) => {
-                // run the predicate &
-                // put the predicate result + the data entry together into container
-                return predicate(data, index)
-                    .pipe(
-                        map((isValid) => ({filterResult: isValid, entry: data})),
-                        subscribeOn(asyncScheduler)
-                    );
-            }
-        ),
-        filterDataAndRemoveContainer()
-    );
+function filterAsyncParallel<T>(
+  predicate: Predicate$<T>,
+  maxConcurrent: number = Number.POSITIVE_INFINITY,
+): MonoTypeOperatorFunction<T> {
+  return pipe(
+    mergeMap((data: T, index: number) => {
+      // run the predicate &
+      // put the predicate result + the data entry together into container
+      return predicate(data, index)
+        .pipe(
+          map((isValid) => ({ filterResult: isValid, entry: data })),
+          subscribeOn(asyncScheduler),
+        );
+    }),
+    filterDataAndRemoveContainer(),
+  );
 }
 
-function filterDataAndRemoveContainer<T>(): OperatorFunction<FilterContainer<T>, T> {
-    return pipe(
-        // Filter the container object synchronously according to the filterResult property
-        filter(data => data.filterResult === true),
-        // remove the data container object from the observable chain
-        map(data => data.entry)
-    );
+function filterDataAndRemoveContainer<T>(): OperatorFunction<
+  FilterContainer<T>,
+  T
+> {
+  return pipe(
+    // Filter the container object synchronously according to the filterResult property
+    filter((data) => data.filterResult === true),
+    // remove the data container object from the observable chain
+    map((data) => data.entry),
+  );
 }
-
