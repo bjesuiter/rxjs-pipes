c793676cee32d44b49dae273cd6966d0e0189f67
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-04-16 18:03:01 +0200

    add tsup build config for throttle-async-rxjs-pipe

---
+++ packages/throttle-async-rxjs-pipe/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["index.ts"],
+  splitting: true,
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  format: [
+    "cjs",
+    "esm",
+  ],
+});
